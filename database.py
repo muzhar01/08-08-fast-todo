865dd0bcfa1be75023ddb3b082e3c2475b398958
Muzhar Hussain <61931803+muzhar01@users.noreply.github.com>
2024-01-13 09:37:56 +0500

    update db string

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost/dbname"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres@localhost:5432/fasttodo"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
